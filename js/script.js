f42b74cad0bcbb512ac67ed9649192537d13b2ff
Ronnylrsd <ronnylrsd@gmail.com>
2022-04-16 19:44:52 -0300

    versão 3.0

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*Typing animation*/
+var typed = new Typed(".typing", {
+    strings:["Desenvolvedor","Front-End Dev","Back-End Dev","Estudante"],
+    typeSpeed:100,
+    BackSpeed: 50,
+    loop:true
+});
